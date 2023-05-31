cb62ef61d4b25384d1bfc338a034d203470f9db2
cout"vaibhav <85737835+vaibhav1663@users.noreply.github.com>
2023-05-31 10:31:30 +0530

    first

---
+++ main.js
@@ -0,0 +1,4 @@
+import "https://cdn.skypack.dev/pin/@hotwired/turbo@v7.1.0-V83RMQBlYCPK9CvTqQoL/mode=imports/optimized/@hotwired/turbo.js";
+import r from "https://cdn.skypack.dev/@domchristie/turn";
+r.config.experimental.viewTransitions = !0;
+r.start();
